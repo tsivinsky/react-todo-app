784bce78271ed039417870c99991b3cb1c04e00c
Daniil <daniil@tsivinsky.com>
2020-07-05 11:21:47 +0300

    Add handleForm function for preventing form submition

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -2,12 +2,23 @@
 import React from "react";
 
 export default function TodoForm({ onAdd }) {
+  // Function for prevent form submitting
+  function handleForm(e) {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+
+      onAdd(e.target.value);
+
+      e.target.value = "";
+    }
+  }
+
   return (
     <form id="add-todo-form">
       <input
         type="text"
         placeholder="What do you want to do?"
-        onKeyDown={(e) => (e.keyCode === 13 ? onAdd(e) : null)}
+        onKeyDown={handleForm}
       />
     </form>
   );
