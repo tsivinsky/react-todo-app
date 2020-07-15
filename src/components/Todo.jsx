1d7c21c768c21cca584dd0c06281bc9b7c576358
Daniil <daniil@tsivinsky.com>
2020-07-15 13:58:30 +0300

    Remove todo settings div

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,46 +1,28 @@
 // Import dependencies
-import React, { useState } from "react";
-import { CSSTransition } from "react-transition-group";
-import TodoSettings from "./TodoSettings";
-import dotsImage from "../images/dots.svg";
+import React from "react";
+import { SortableHandle } from "react-sortable-hoc";
 
-const completedTodoStyle = {
-  textDecoration: "line-through",
-  opacity: "0.6",
-};
+// Import images
+import Dots from "../images/dots.svg";
+import Trash from "../images/trash.svg";
 
-const todoStyle = {
-  textDecoration: "",
-  opacity: "1",
-};
-
-export default function Todo({ id, text, completed, onComplete, onDelete }) {
-  const [showSettings, setShowSettings] = useState(false);
+export default function Todo({ id, text, completed, onComplete, onRemove }) {
+  const DragHandler = SortableHandle(() => (
+    <img src={Dots} className="btn-todo-drag" alt="Dots button" />
+  ));
 
   return (
-    <div className="todo">
-      <h3
-        style={completed ? completedTodoStyle : todoStyle}
-        className="todo-text"
-        onClick={() => onComplete(id)}
-      >
+    <div className={`todo ${completed ? "todo-completed" : ""}`}>
+      <DragHandler />
+      <h3 className="todo-text" onClick={() => onComplete(id)}>
         {text}
       </h3>
-      <div
-        className="settings"
-        onClick={() => setShowSettings((prev) => !prev)}
-      >
-        <CSSTransition
-          in={showSettings}
-          timeout={300}
-          classNames="todo-settings"
-          unmountOnExit
-        >
-          <TodoSettings id={id} onComplete={onComplete} onDelete={onDelete} />
-        </CSSTransition>
-
-        <img src={dotsImage} alt="Dots" className="dots-button" />
-      </div>
+      <img
+        src={Trash}
+        className="trashbin"
+        onClick={() => onRemove(id)}
+        alt="Trashbin"
+      />
     </div>
   );
 }
