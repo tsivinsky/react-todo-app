b9f6c7af87a6bce8ff53142a330cfc7c220fdd5c
Daniil <daniil@tsivinsky.com>
2020-07-15 21:31:57 +0300

    Change way to store tasks, instead of one array many projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,52 @@
 // Import dependencies
-import React from "react";
-import useTodos from "./hooks/useTodos";
+import React, { useState, useEffect } from "react";
+import useProjects from "./hooks/useProjects";
 import { SortableContainer, SortableElement } from "react-sortable-hoc";
+import queryString from "query-string";
 
 // Import components
 import TodoForm from "./components/TodoForm";
 import Todo from "./components/Todo";
 
 export default function App() {
-  // Global state
-  const [todos, addTodo, completeTodo, removeTodo, moveTodo] = useTodos();
+  // Global store
+  const [
+    projects,
+    { addTodo, completeTodo, removeTodo, moveTodo },
+  ] = useProjects();
+
+  const params = queryString.parse(window.location.search);
+
+  // Local states
+  const [project, setProject] = useState(() => {
+    if (params.id) {
+      return projects.find((project) => project._id === params.id);
+    } else {
+      return projects[0];
+    }
+  });
+
+  useEffect(() => {
+    if (params.id) {
+      setProject(projects.find((project) => project._id === params.id));
+    } else {
+      setProject(projects[0]);
+    }
+  }, [params.id, projects]);
 
   const TodoWrapper = SortableElement(({ id, text, completed }) => (
     <Todo
       id={id}
       text={text}
       completed={completed}
-      onComplete={(id) => completeTodo(id)}
-      onRemove={(id) => removeTodo(id)}
+      onComplete={(id) => completeTodo(project._id, id)}
+      onRemove={(id) => removeTodo(project._id, id)}
     />
   ));
 
   const Todolist = SortableContainer(() => (
     <div className="todos">
-      {todos.map((todo, i) => (
+      {project.tasks.map((todo, i) => (
         <TodoWrapper
           key={i}
           index={i}
@@ -38,18 +61,20 @@ export default function App() {
   return (
     <>
       <header>
-        <h1 className="title">Todo App</h1>
-        <TodoForm onAdd={(value) => addTodo(value)} />
+        <h1 className="title">{project.name}</h1>
+        <TodoForm onAdd={(value) => addTodo(project._id, value)} />
       </header>
       <main>
-        {todos.length === 0 ? (
+        {project.tasks.length === 0 ? (
           <h2 className="no-todos-message">
             You do not have any tasks. <br />
             It`s a freetime! ⌛
           </h2>
         ) : (
           <Todolist
-            onSortEnd={({ oldIndex, newIndex }) => moveTodo(oldIndex, newIndex)}
+            onSortEnd={({ oldIndex, newIndex }) =>
+              moveTodo(project._id, oldIndex, newIndex)
+            }
             lockAxis="y"
             helperClass="todo-dragging"
             useDragHandle
