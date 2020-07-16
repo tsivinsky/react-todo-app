2a7da17246027e51392918dfb135c5bc26fc4cb8
Daniil <daniil@tsivinsky.com>
2020-07-16 14:42:27 +0300

    Add renderer for text in tasks (#7)

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,24 +1,56 @@
 // Import dependencies
 import React from "react";
 import { Twemoji } from "react-emoji-render";
+import validator from "validator";
 
 // Import images
 import Dots from "../images/dots.svg";
+import Checkbox from "../images/checkbox.svg";
 import Trash from "../images/trash.svg";
 
 export default function Todo({ id, text, completed, onComplete, onRemove }) {
+  function Word({ text }) {
+    if (validator.isURL(text)) {
+      if (!text.startsWith("http")) {
+        text = `https://${text}`;
+      }
+
+      return (
+        <a href={text} target="_blank">
+          {text}
+        </a>
+      );
+    }
+
+    if (text.startsWith(":")) return <Twemoji text={text} />;
+
+    return text;
+  }
+
   return (
     <div className={`todo ${completed ? "todo-completed" : ""}`}>
       <img src={Dots} className="btn-todo-drag" alt="Dots button" />
-      <h3 className="todo-text" onClick={() => onComplete(id)}>
-        <Twemoji text={text} />
+      <h3 className="todo-text">
+        {text.split(" ").map((word, i) => (
+          <React.Fragment key={i}>
+            <Word text={word} />{" "}
+          </React.Fragment>
+        ))}
       </h3>
-      <img
-        src={Trash}
-        className="trashbin"
-        onClick={() => onRemove(id)}
-        alt="Trashbin"
-      />
+      <div className="buttons">
+        <img
+          src={Checkbox}
+          className="checkbox"
+          onClick={() => onComplete(id)}
+          alt="Checkbox button"
+        />
+        <img
+          src={Trash}
+          className="trashbin"
+          onClick={() => onRemove(id)}
+          alt="Trashbin"
+        />
+      </div>
     </div>
   );
 }
