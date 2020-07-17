1a07a4b340ed8876960288d8c1abd7352963d536
Daniil <daniil@tsivinsky.com>
2020-07-17 20:33:49 +0300

    Change DOM structure of task

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,13 +2,21 @@
 import React from "react";
 import { Twemoji } from "react-emoji-render";
 import validator from "validator";
+import { useSwipeable } from "react-swipeable";
 
 // Import images
 import Dots from "../images/dots.svg";
-import Checkbox from "../images/checkbox.svg";
 import Trash from "../images/trash.svg";
 
 export default function Todo({ id, text, completed, onComplete, onRemove }) {
+  const handlers = useSwipeable({
+    // Event for deleting task
+    onSwipedLeft: () => onComplete(id),
+    onSwipedRight: () => onRemove(id),
+    preventDefaultTouchmoveEvent: true,
+    trackMouse: true,
+  });
+
   function Word({ text }) {
     if (validator.isURL(text)) {
       if (!text.startsWith("http")) {
@@ -17,7 +25,7 @@ export default function Todo({ id, text, completed, onComplete, onRemove }) {
 
       return (
         <a href={text} target="_blank" rel="noopener noreferrer">
-          {text}
+          Link
         </a>
       );
     }
@@ -28,29 +36,23 @@ export default function Todo({ id, text, completed, onComplete, onRemove }) {
   }
 
   return (
-    <div className={`todo ${completed ? "todo-completed" : ""}`}>
+    <div className={`todo ${completed ? "todo-completed" : ""}`} {...handlers}>
       <img src={Dots} className="btn-todo-drag" alt="Dots button" />
-      <h3 className="todo-text">
-        {text.split(" ").map((word, i) => (
-          <React.Fragment key={i}>
-            <Word text={word} />{" "}
-          </React.Fragment>
-        ))}
-      </h3>
-      <div className="buttons">
-        <img
-          src={Checkbox}
-          className="checkbox"
-          onClick={() => onComplete(id)}
-          alt="Checkbox button"
-        />
-        <img
-          src={Trash}
-          className="trashbin"
-          onClick={() => onRemove(id)}
-          alt="Trashbin"
-        />
+      <div className="inner-block">
+        <h3 className="todo-text">
+          {text.split(" ").map((word, i) => (
+            <React.Fragment key={i}>
+              <Word text={word} />{" "}
+            </React.Fragment>
+          ))}
+        </h3>
       </div>
+      <img
+        src={Trash}
+        className="trashbin"
+        onClick={() => onRemove(id)}
+        alt="Trashbin"
+      />
     </div>
   );
 }
